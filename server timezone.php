2f89a024d8b21941f7543e96049fe4dc0cbb5d59
Nusaniz <67667654+nusaniz@users.noreply.github.com>
2021-08-11 11:29:35 +0700

    Update server timezone.php

--- server timezone.php
+++ server timezone.php
@@ -1,2 +1,5 @@
 $timezone = date_default_timezone_get();
 echo "The current server timezone is: " . $timezone;
+
+
+src: https://stackoverflow.com/questions/470617/how-do-i-get-the-current-date-and-time-in-php
